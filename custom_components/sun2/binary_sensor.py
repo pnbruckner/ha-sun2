43b15cb6cbcbe533754c415a75705e989f435f2d
Phil Bruckner <pnbruckner@gmail.com>
2025-12-08 08:51:14 -0600

    Refactor elevation binary sensor _update method

--- custom_components/sun2/binary_sensor.py
+++ custom_components/sun2/binary_sensor.py
@@ -55,6 +55,32 @@ class Sun2ElevationSensor(Sun2EntityWithElvAdjs, BinarySensorEntity):
 
     def _update(self, cur_dttm: datetime) -> None:
         """Update state."""
+        self._attr_is_on = self._get_cur_state(cur_dttm)
+        self._attr_icon = ICON_ABOVE if self._attr_is_on else ICON_BELOW
+
+        if nxt_chg := self._get_nxt_chg():
+            self._schedule_update(nxt_chg)
+            nxt_chg = self._as_tz(nxt_chg)
+            # It's ok that nxt_chg is now in location's time zone and cur_dttm is in
+            # UTC. nxt_chg's value will be automatically converted to UTC during the
+            # subtraction operation.
+            if nxt_chg - cur_dttm > ONE_DAY and self.hass.state == CoreState.running:
+                LOGGER.warning(
+                    "%s: Sun elevation will not reach %f again until %s",
+                    self._log_name,
+                    self._threshold,
+                    nxt_chg.date(),
+                )
+        elif self.hass.state == CoreState.running:
+            LOGGER.error(
+                "%s: Sun elevation never reaches %f at this location",
+                self._log_name,
+                self._threshold,
+            )
+        self._attr_extra_state_attributes = {ATTR_NEXT_CHANGE: nxt_chg}
+
+    def _get_cur_state(self, cur_dttm: datetime) -> bool:
+        """Get current sensor state."""
         if self._first_update:
             if (nxt_chg := self._time_at_elevation(self._threshold)) is None:
                 # Sun doesn't cross threshold today. Base current state on solar
@@ -62,25 +88,21 @@ class Sun2ElevationSensor(Sun2EntityWithElvAdjs, BinarySensorEntity):
                 # solar elevation, round current time to nearest second.
                 cur_elv = self._solar_elevation(nearest_second(cur_dttm))
                 if self._rising:
-                    self._attr_is_on = cur_elv >= self._threshold - self._ris_elv_adj
-                else:
-                    self._attr_is_on = cur_elv <= self._threshold - self._set_elv_adj
-            else:
-                # Sun does cross threshold today.
-                if cur_dttm < nxt_chg:
-                    # Sun has not yet crossed threshold on current part of the "solar
-                    # elevation curve." Set state parameters to be on previous part of
-                    # the curve so current state and next change are determined
-                    # correctly.
-                    self._rising = not self._rising
-                    if not self._rising:
-                        self._dt -= ONE_DAY
-                self._attr_is_on = self._rising
-        else:
-            nxt_chg = None
-            self._attr_is_on = self._rising
-        self._attr_icon = ICON_ABOVE if self._attr_is_on else ICON_BELOW
+                    return cur_elv >= self._threshold - self._ris_elv_adj
+                return cur_elv <= self._threshold - self._set_elv_adj
+            # Sun does cross threshold today.
+            if cur_dttm < nxt_chg:
+                # Sun has not yet crossed threshold on current part of the "solar
+                # elevation curve." Set state parameters to be on previous part of
+                # the curve so current state and next change are determined
+                # correctly.
+                self._rising = not self._rising
+                if not self._rising:
+                    self._dt -= ONE_DAY
+        return self._rising
 
+    def _get_nxt_chg(self) -> datetime | None:
+        """Get next time sun crosses threshold."""
         # Find next time sun crosses threshold. Note that it's possible that might not
         # happen today, or even tomorrow, depending on location & time of year. Move to
         # next part of solar elevation curve, and if that doesn't cross threshold, keep
@@ -91,30 +113,8 @@ class Sun2ElevationSensor(Sun2EntityWithElvAdjs, BinarySensorEntity):
             if self._rising:
                 self._dt += ONE_DAY
             if nxt_chg := self._time_at_elevation(self._threshold):
-                self._schedule_update(nxt_chg)
-                nxt_chg = self._as_tz(nxt_chg)
-                # It's ok that nxt_chg is now in location's time zone and cur_dttm is in
-                # UTC. nxt_chg's value will be automatically converted to UTC during the
-                # subtraction operation.
-                if (
-                    nxt_chg - cur_dttm > ONE_DAY
-                    and self.hass.state == CoreState.running
-                ):
-                    LOGGER.warning(
-                        "%s: Sun elevation will not reach %f again until %s",
-                        self._log_name,
-                        self._threshold,
-                        nxt_chg.date(),
-                    )
-                break
-        else:
-            if self.hass.state == CoreState.running:
-                LOGGER.error(
-                    "%s: Sun elevation never reaches %f at this location",
-                    self._log_name,
-                    self._threshold,
-                )
-        self._attr_extra_state_attributes = {ATTR_NEXT_CHANGE: nxt_chg}
+                return nxt_chg
+        return None
 
 
 class Sun2BinarySensorEntrySetup(Sun2EntrySetup):
